83c39c0d9383e7846105e27566139a7915fb0ea3
Ethan Kent <ekent@jd11.law.harvard.edu>
2024-02-26 08:30:25 -0500

    moving towards ReaderT

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,8 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
 import Api (createRequest)
 import Configuration.Dotenv (defaultConfig, loadFile)
+import Control.Monad.Reader (MonadIO (liftIO), MonadReader (ask), ReaderT (runReaderT), asks)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.Vector (Vector)
@@ -16,11 +15,32 @@ import Network.HTTP.Simple (
  )
 import Types
 
+type App a = ReaderT Config IO a
+
+data Config = Config {apiUrl :: ByteString, apiKey :: ByteString}
+
+loadConfig :: IO Config
+loadConfig = Config <$> getOpenChargeApiUrl <*> getOpenChargeApiKey
+
+runApp :: App ()
+runApp = do
+  config <- ask
+  let request = createRequest (apiUrl config) (apiKey config)
+  response <- liftIO $ httpJSONEither request :: App (Response (Either JSONException (Vector Poi)))
+  liftIO $ print $ getResponseBody response
+
 main :: IO ()
 main = do
-  loadFile defaultConfig
-  apiUrl <- getOpenChargeApiUrl
-  apiKey <- getOpenChargeApiKey
-  let request = createRequest apiUrl apiKey
-  response <- httpJSONEither request :: IO (Response (Either JSONException (Vector Poi)))
-  print $ getResponseBody response
+  loadConfig >>= runReaderT runApp
+
+-- response <- liftIO $ httpJSONEither request
+-- liftIO $ print $ getResponseBody response
+
+-- main :: IO ()
+-- main = do
+--   loadFile defaultConfig
+--   apiUrl <- getOpenChargeApiUrl
+--   apiKey <- getOpenChargeApiKey
+--   let request = createRequest apiUrl apiKey
+--   response <- httpJSONEither request :: IO (Response (Either JSONException (Vector Poi)))
+--   print $ getResponseBody response
