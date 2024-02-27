0e829fcf891edb737829ea7ef9201f316779a56a
Ethan Kent <ekent@jd11.law.harvard.edu>
2024-02-26 20:36:00 -0500

    adapt api call to use ReaderT

--- app/Api.hs
+++ app/Api.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Api where
 
+import Control.Monad.Reader (MonadReader (ask), ReaderT)
 import Data.ByteString (ByteString)
-import Network.HTTP.Client.Conduit (Request)
+import Data.Vector (Vector)
+import Network.HTTP.Client.Conduit (Request, Response)
 import Network.HTTP.Simple (
+  JSONException,
   defaultRequest,
+  getResponseBody,
+  httpJSONEither,
   setRequestHeader,
   setRequestHost,
   setRequestPath,
@@ -13,16 +19,44 @@ import Network.HTTP.Simple (
   setRequestQueryString,
   setRequestSecure,
  )
+import Types (Poi)
 
-createRequest :: ByteString -> ByteString -> Request
-createRequest apiUrl apiKey =
-  setRequestSecure True $
-    setRequestPort port $
-      setRequestPath requestPath $
-        setRequestQueryString queryString $
-          setRequestHeader "X-API-Key" [apiKey] $
-            setRequestHost apiUrl defaultRequest
+data CreateRequestDeps = CreateRequestDeps
+  { apiUrl :: ByteString
+  , apiKey :: ByteString
+  }
+
+createRequest :: ReaderT CreateRequestDeps IO Request
+createRequest = do
+  CreateRequestDeps{apiUrl, apiKey} <- ask
+  return $
+    setRequestSecure True $
+      setRequestPort port $
+        setRequestPath requestPath $
+          setRequestQueryString queryString $
+            setRequestHeader "X-API-Key" [apiKey] $
+              setRequestHost apiUrl defaultRequest
  where
   port = 443
   requestPath = "/v3/poi/"
-  queryString = [("output", Just "json"), ("countrycode", Just "US"), ("maxresults", Just "10")]
+  queryString =
+    [ ("output", Just "json")
+    , ("countrycode", Just "US")
+    , ("maxresults", Just "10")
+    ]
+
+performRequest ::
+  ReaderT
+    CreateRequestDeps
+    IO
+    (Response (Either JSONException (Vector Poi)))
+performRequest = do
+  request <- createRequest
+  httpJSONEither request
+
+getPoi ::
+  ReaderT
+    CreateRequestDeps
+    IO
+    (Either JSONException (Vector Poi))
+getPoi = getResponseBody <$> performRequest
--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,16 @@
 module Main where
 
-import Api (createRequest)
+import Api (CreateRequestDeps (CreateRequestDeps), getPoi)
 import Configuration.Dotenv (defaultConfig, loadFile)
-import Control.Monad.Reader (MonadIO (liftIO), MonadReader (ask), ReaderT (runReaderT), asks)
-import Data.ByteString (ByteString)
-import qualified Data.ByteString.Lazy.Char8 as L8
-import Data.Vector (Vector)
+import Control.Monad.Reader (ReaderT (runReaderT))
 import Env (getOpenChargeApiKey, getOpenChargeApiUrl)
-import Network.HTTP.Conduit (Request (requestBody), Response (responseBody))
-import Network.HTTP.Simple (
-  JSONException,
-  getResponseBody,
-  httpJSONEither,
- )
-import Types
 
-type App a = ReaderT Config IO a
-
-data Config = Config {apiUrl :: ByteString, apiKey :: ByteString}
-
-loadConfig :: IO Config
-loadConfig = Config <$> getOpenChargeApiUrl <*> getOpenChargeApiKey
-
-runApp :: App ()
-runApp = do
-  config <- ask
-  let request = createRequest (apiUrl config) (apiKey config)
-  response <- liftIO $ httpJSONEither request :: App (Response (Either JSONException (Vector Poi)))
-  liftIO $ print $ getResponseBody response
+loadConfig :: IO CreateRequestDeps
+loadConfig = CreateRequestDeps <$> getOpenChargeApiUrl <*> getOpenChargeApiKey
 
 main :: IO ()
 main = do
   loadFile defaultConfig
-  loadConfig >>= runReaderT runApp
-
--- response <- liftIO $ httpJSONEither request
--- liftIO $ print $ getResponseBody response
-
--- main :: IO ()
--- main = do
---   loadFile defaultConfig
---   apiUrl <- getOpenChargeApiUrl
---   apiKey <- getOpenChargeApiKey
---   let request = createRequest apiUrl apiKey
---   response <- httpJSONEither request :: IO (Response (Either JSONException (Vector Poi)))
---   print $ getResponseBody response
+  config <- loadConfig
+  result <- runReaderT getPoi config
+  print result
